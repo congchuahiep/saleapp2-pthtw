f56ddac806755310a11c01f749ea9f016d7b9d8f
congchuahiep <congchuahiep@gmail.com>
2025-03-30 15:11:48 +0700

    add pojo classes and repository classes, install spring orm

---
+++ src/main/java/com/thh/repositories/impl/CategoryRepositoryImpl.java
@@ -0,0 +1,26 @@
+///*
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+// */
+//package com.thh.repositories.impl;
+//
+//import com.thh.pojo.Category;
+//import com.thh.saleapp.HibernateUtils;
+//import org.hibernate.query.Query;
+//import org.hibernate.Session;
+//
+//import java.util.List;
+//
+///**
+// *
+// * @author admin
+// */
+//public class CategoryRepositoryImpl {
+//    public List<Category> getCates() {
+//        try (Session s = HibernateUtils.getFACTORY().openSession()) {
+//            Query<Category> q = s.createQuery("FROM Category", Category.class);
+//            
+//            return q.getResultList();
+//        }
+//    }
+//}
---
+++ src/main/java/com/thh/repositories/impl/StatsRepositoryImpl.java
@@ -0,0 +1,40 @@
+///*
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+// */
+//package com.thh.repositories.impl;
+//
+//import com.thh.pojo.OrderDetail;
+//import com.thh.pojo.Product;
+//import com.thh.saleapp.HibernateUtils;
+//import jakarta.persistence.criteria.*;
+//import org.hibernate.Session;
+//import org.hibernate.query.Query;
+//
+//import java.util.List;
+//
+///**
+// *
+// * @author admin
+// */
+//public class StatsRepositoryImpl {
+//    public List<Object[]> statsRevenueByProduct() {
+//        try (Session s = HibernateUtils.getFACTORY().openSession()) {
+//            CriteriaBuilder b = s.getCriteriaBuilder();
+//            CriteriaQuery<Object[]> q = b.createQuery(Object[].class);
+//            Root<OrderDetail> root = q.from(OrderDetail.class);
+//            Join<OrderDetail, Product> join = root.join("productId", JoinType.RIGHT);
+//            
+//            q.multiselect(
+//                    join.get("id"), 
+//                    join.get("name"),
+//                    b.sum(b.prod(root.get("unitPrice"), root.get("quantity")))
+//            );
+//            
+//            q.groupBy(join.get("id"));
+//            
+//            Query<Object[]> query = s.createQuery(q);
+//            return query.getResultList();
+//        }
+//    }
+//}
